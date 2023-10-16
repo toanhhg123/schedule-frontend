8ac10a01d1e3f7eb63ec0af50c6db84ae2a00c51
giadat@linux <contact.vgdat@gmail.com>
2023-10-16 19:18:41 +0700

    fix sidebar

--- src/pages/client/DashBoard.tsx
+++ src/pages/client/DashBoard.tsx
@@ -1,15 +1,25 @@
-import { theme } from 'antd'
+import { Layout, theme } from 'antd'
 
 const DashBoard = () => {
+  const { Content } = Layout
   const {
     token: { colorBgContainer }
   } = theme.useToken()
 
   return (
     <>
-      <div style={{ padding: 24, textAlign: 'center', background: colorBgContainer }}>
-        <p>DashBoard</p>
-      </div>
+      <Content
+        style={{
+          padding: 0,
+          margin: '16px 12px',
+          marginTop: 72,
+          background: colorBgContainer
+        }}
+      >
+        <div style={{ padding: 24, textAlign: 'center', background: colorBgContainer }}>
+          <p>DashBoard</p>
+        </div>
+      </Content>
     </>
   )
 }
