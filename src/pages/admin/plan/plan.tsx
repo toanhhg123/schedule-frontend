15a0be6d4edaf89b1a6633a64ddcee39061f5f02
toanhhg123 <toanhhg123@gmail.com>
2023-10-18 03:34:06 +0700

    add plan page

--- src/pages/admin/plan/plan.tsx
+++ src/pages/admin/plan/plan.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { getAllPlan } from '@/app/features/plan/plan.service'
+import { Plan } from '@/app/features/plan/plan.type'
+import Layout from '@/components/layout'
+import { Table, message } from 'antd'
+import { useEffect, useState } from 'react'
 
-const plan = () => {
-  return <div>plan</div>
+const Plan = () => {
+  const [plans, setPlans] = useState<Plan[]>([])
+
+  const getInitPlan = () => {
+    message.loading({ type: 'loading', content: 'Action in progress..' })
+    getAllPlan().then((data) => {
+      setPlans(data)
+      message.destroy()
+    })
+  }
+
+  useEffect(() => {
+    getInitPlan()
+  }, [])
+
+  return (
+    <Layout>
+      <Table dataSource={plans} />
+    </Layout>
+  )
 }
 
-export default plan
+export default Plan
