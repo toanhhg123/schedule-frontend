c7ae12f25e11edbe1518cc560de2b6c541b9d979
giadat@linux <contact.vgdat@gmail.com>
2023-10-17 23:29:14 +0700

    done sidebar

---
+++ src/pages/client/information.tsx
@@ -0,0 +1,15 @@
+import LayoutClient from '@/components/client/layoutClient'
+
+const Information = () => {
+  return (
+    <LayoutClient
+      content={
+        <>
+          <p>Information</p>
+        </>
+      }
+    />
+  )
+}
+
+export default Information
---
+++ src/pages/client/team.tsx
@@ -0,0 +1,15 @@
+import LayoutClient from '@/components/client/layoutClient'
+
+const Team = () => {
+  return (
+    <LayoutClient
+      content={
+        <>
+          <p>Team</p>
+        </>
+      }
+    />
+  )
+}
+
+export default Team
